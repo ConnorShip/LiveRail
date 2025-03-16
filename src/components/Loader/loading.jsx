abc07f0af1f506e85ac5592c06364f502c62d73e
Connor Shipley <connorshipley9@gmail.com>
2025-03-16 12:23:40 +0000

    load page test

---
+++ src/components/Loader/loading.jsx
@@ -0,0 +1,16 @@
+
+function LoadPage () {
+    return (
+        <div id="body">
+            <div className="stage">
+                <div className="container">
+                    <div className="image box"></div>
+                    <div className="load box">
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
